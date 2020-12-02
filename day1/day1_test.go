cfa56b9fda52e59299cd8273e658d6e26a5f5921
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2020-12-02 16:51:41 +0100

    update day1

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -24,6 +24,7 @@ func TestSolveDay1Part1(t *testing.T) {
 		}
 	})
 }
+
 func TestSolveDay1Part2(t *testing.T) {
 	t.Run("Test SolveDay1Part2 with valid list", func(t *testing.T) {
 		got := SolveDay1Part2([]int{1721, 979, 366, 299, 675, 1456})
@@ -45,7 +46,6 @@ func TestSolveDay1Part2(t *testing.T) {
 }
 
 func BenchmarkSolveDay1Part1(b *testing.B) {
-	// run the Fib function b.N times
 	i, _ := ioutil.ReadFile("input.txt")
 	input := listToSlice(string(i))
 	for n := 0; n < b.N; n++ {
@@ -54,7 +54,6 @@ func BenchmarkSolveDay1Part1(b *testing.B) {
 }
 
 func BenchmarkSolveDay1Part2(b *testing.B) {
-	// run the Fib function b.N times
 	i, _ := ioutil.ReadFile("input.txt")
 	input := listToSlice(string(i))
 	for n := 0; n < b.N; n++ {
@@ -63,7 +62,6 @@ func BenchmarkSolveDay1Part2(b *testing.B) {
 }
 
 func TestListToSlice(t *testing.T) {
-
 	t.Run("Test ListToSlice with valid list", func(t *testing.T) {
 		s := `1721
 979
@@ -78,7 +76,6 @@ func TestListToSlice(t *testing.T) {
 			t.Errorf("expected '%d' but got '%d'", expected, got)
 		}
 	})
-
 	t.Run("Test ListToSlice with invalid list", func(t *testing.T) {
 		s := `1721
 979
