ef98ac9999979042660481845c56b097cab37d2b
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2020-12-09 12:14:35 +0100

    add day9

---
+++ day9/day9.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	i, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	input := string(i)
+	fmt.Printf("Part 1: %v\n", SolveDay9Part1(intListToSlice(input), 25))
+	fmt.Printf("Part 2: %v\n", SolveDay9Part2(intListToSlice(input), 25))
+}
+
+//SolveDay9Part1 returns the first value that can not reached with a sum of 2 values of the last $start values
+func SolveDay9Part1(i []int, start int) (s int) {
+	cur := start
+	if start > len(i) || start < 2 {
+		return 0
+	}
+	for {
+		found := false
+		if len(i) == cur {
+			break
+		}
+		for _, num := range i[cur-start : cur] {
+			if found {
+				break
+			}
+			for _, num2 := range i[cur-start : cur] {
+				if num+num2 == i[cur] && num != num2 {
+					found = true
+					break
+				}
+			}
+		}
+		if found {
+			cur++
+			continue
+		}
+		return i[cur]
+	}
+	return 0
+}
+
+//SolveDay9Part2 search for 5 numbers that sum is the value from SolveDay9Part1 and sum the lowest and highest
+func SolveDay9Part2(i []int, start int) (s int) {
+	numToSolve := SolveDay9Part1(i, start)
+	var index, currentIndex, currentSum int
+	var currentNumbers []int
+	for {
+		currentSum += i[currentIndex]
+		currentNumbers = append(currentNumbers, i[currentIndex])
+		if currentSum == numToSolve {
+			break
+		}
+		if currentSum > numToSolve {
+			index++
+			currentIndex = index
+			currentSum = 0
+			currentNumbers = []int{}
+			continue
+		}
+		currentIndex++
+	}
+	sort.Ints(currentNumbers)
+	return currentNumbers[0] + currentNumbers[len(currentNumbers)-1]
+
+}
+
+//Helper functions
+//stringListToSlice converts the list of strings (each string one row) to a slice
+func stringListToSlice(list string) (s []string) {
+	for _, line := range strings.Split(strings.TrimSuffix(list, "\n"), "\n") {
+		s = append(s, line)
+	}
+	return
+}
+
+//intListToSlice converts the list of numbers (each number one row) to a slice
+func intListToSlice(list string) (i []int) {
+	for _, line := range strings.Split(strings.TrimSuffix(list, "\n"), "\n") {
+		lineInt, err := strconv.Atoi(line)
+		if err != nil {
+			return nil
+		}
+		i = append(i, lineInt)
+	}
+	return
+}
---
+++ day9/day9_test.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestSolveDay9Part1(t *testing.T) {
+	t.Run("Test SolveDay9Part1", func(t *testing.T) {
+		i := intListToSlice(`35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+`)
+		got := SolveDay9Part1(i, 5)
+		expected := 127
+
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+	t.Run("Test SolveDay9Part1 infinite loop", func(t *testing.T) {
+		i := intListToSlice(`1
+2
+3`)
+		got := SolveDay9Part1(i, 2)
+		expected := 0
+
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+
+	t.Run("Test SolveDay9Part1 invalid input", func(t *testing.T) {
+		i := intListToSlice(`1`)
+		got := SolveDay9Part1(i, 2)
+		expected := 0
+
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+	t.Run("Test SolveDay9Part1 invalid input", func(t *testing.T) {
+		i := intListToSlice(`1
+2`)
+		got := SolveDay9Part1(i, 1)
+		expected := 0
+
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+}
+
+func BenchmarkSolveDay9Part1(b *testing.B) {
+	i, _ := ioutil.ReadFile("input.txt")
+	input := intListToSlice(string(i))
+	for n := 0; n < b.N; n++ {
+		_ = SolveDay9Part1(input, 25)
+	}
+}
+
+func TestSolveDay9Part2(t *testing.T) {
+	t.Run("Test SolveDay9Part2", func(t *testing.T) {
+		i := intListToSlice(`35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+`)
+		got := SolveDay9Part2(i, 5)
+		expected := 62
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+}
+
+func BenchmarkSolveDay9Part2(b *testing.B) {
+	i, _ := ioutil.ReadFile("input.txt")
+	input := intListToSlice(string(i))
+	for n := 0; n < b.N; n++ {
+		_ = SolveDay9Part2(input, 5)
+	}
+}
+
+func TestStringListToSlice(t *testing.T) {
+	t.Run("Test stringListToSlice with valid list", func(t *testing.T) {
+		s := `string
+string 2
+string:3`
+		got := stringListToSlice(s)
+		expected := []string{"string", "string 2", "string:3"}
+		if !equalString(got, expected) {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+}
+
+func TestIntListToSlice(t *testing.T) {
+	t.Run("Test intListToSlice with valid list", func(t *testing.T) {
+		s := `123
+1234
+000
+00123`
+		got := intListToSlice(s)
+		expected := []int{123,1234,0,123}
+		if !equalInt(got, expected) {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+	t.Run("Test intListToSlice with invalid list", func(t *testing.T) {
+		s := `123
+1234a
+000
+00123`
+		got := intListToSlice(s)
+		expected := []int{}
+		if !equalInt(got, expected) {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+}
+
+func equalString(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func equalInt(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
