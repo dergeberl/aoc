70f001e69cf9b80ffeb7bc7eed50cfc779f194c3
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2020-12-20 10:18:25 +0100

    add day19

---
+++ day19/day19.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var loopDetect map[int]int
+var cache map[int]string
+
+func main() {
+	i, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	input := string(i)
+
+	fmt.Printf("Part 1: %v\n", SolveDay19Part1(input))
+	fmt.Printf("Part 2: %v\n", SolveDay19Part2(input))
+}
+
+func init() {
+	loopDetect = make(map[int]int)
+}
+
+//SolveDay19Part1 return the number of strings that matched the rules
+func SolveDay19Part1(input string) (s int) {
+	blocks := strings.Split(input, "\n\n")
+	if len(blocks) != 2 {
+		return 0
+	}
+	rules := make(map[int]string)
+	for _, line := range strings.Split(blocks[0], "\n") {
+		splitLine := strings.Split(line, ": ")
+		ruleId, _ := strconv.Atoi(splitLine[0])
+		rules[ruleId] = splitLine[1]
+
+	}
+	cache = make(map[int]string)
+	regex := "^" + resolveRulesRegex(0, rules) + "$"
+	r, _ := regexp.Compile(regex)
+	for _, line := range strings.Split(blocks[1], "\n") {
+		if r.MatchString(line) {
+			s++
+		}
+	}
+	return
+}
+
+//SolveDay19Part2 return the number of strings that matched the rules (with updated rule 8 und 11)
+func SolveDay19Part2(input string) (s int) {
+	blocks := strings.Split(input, "\n\n")
+	if len(blocks) != 2 {
+		return 0
+	}
+	rules := make(map[int]string)
+	for _, line := range strings.Split(blocks[0], "\n") {
+		splitLine := strings.Split(line, ": ")
+		ruleId, _ := strconv.Atoi(splitLine[0])
+		rules[ruleId] = splitLine[1]
+
+	}
+	rules[8] = "42 | 42 8"
+	rules[11] = "42 31 | 42 11 31"
+	cache = make(map[int]string)
+	regex := "^" + resolveRulesRegex(0, rules) + "$"
+	r, _ := regexp.Compile(regex)
+	for _, line := range strings.Split(blocks[1], "\n") {
+		if r.MatchString(line) {
+			s++
+		}
+	}
+	return
+}
+
+//resolveRulesRegex resolve a rule to a regex string (follow a loop max 20 times)
+func resolveRulesRegex(ruleID int, rules map[int]string) (resolvedRule string) {
+	if cache[ruleID] != "" {
+		return cache[ruleID]
+	}
+	if strings.HasPrefix(rules[ruleID], "\"") {
+		return strings.Trim(rules[ruleID], "\"")
+	}
+	regex := "(:?"
+	orBlocks := strings.Split(rules[ruleID], " | ")
+	for i, ruleBlock := range orBlocks {
+		if i != 0 {
+			regex += "|"
+		}
+		for _, otherRules := range strings.Split(ruleBlock, " ") {
+			otherRulesNum, _ := strconv.Atoi(otherRules)
+			if otherRulesNum == ruleID {
+				loopDetect[ruleID]++
+				if loopDetect[ruleID] > 20 {
+					continue
+				}
+			}
+			regex += resolveRulesRegex(otherRulesNum, rules)
+		}
+	}
+	regex += ")"
+	cache[ruleID] = regex
+	return regex
+}
+
+//Helper functions
+//stringListToSlice converts the list of strings (each string one row) to a slice
+func stringListToSlice(list string) (s []string) {
+	for _, line := range strings.Split(strings.TrimSuffix(list, "\n"), "\n") {
+		s = append(s, line)
+	}
+	return
+}
+
+//intListToSlice converts the list of numbers (each number one row) to a slice
+func intListToSlice(list string) (i []int) {
+	for _, line := range strings.Split(strings.TrimSuffix(list, "\n"), "\n") {
+		lineInt, err := strconv.Atoi(line)
+		if err != nil {
+			return nil
+		}
+		i = append(i, lineInt)
+	}
+	return
+}
---
+++ day19/day19_test.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestSolveDay19Part1(t *testing.T) {
+	t.Run("Test SolveDay19Part1", func(t *testing.T) {
+		i := `0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: "a"
+5: "b"
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb`
+		got := SolveDay19Part1(i)
+		expected := 2
+
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+	t.Run("Test SolveDay19Part1", func(t *testing.T) {
+		i := `0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: "a"
+5: "b"
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb`
+		got := SolveDay19Part1(i)
+		expected := 0
+
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+}
+
+func BenchmarkSolveDay19Part1(b *testing.B) {
+	i, _ := ioutil.ReadFile("input.txt")
+	input := string(i)
+	for n := 0; n < b.N; n++ {
+		_ = SolveDay19Part1(input)
+	}
+}
+
+func TestSolveDay19Part2(t *testing.T) {
+	t.Run("Test SolveDay19Part2", func(t *testing.T) {
+		i := `42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba`
+		got := SolveDay19Part2(i)
+		expected := 12
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+	t.Run("Test SolveDay19Part2", func(t *testing.T) {
+		i := `42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba`
+		got := SolveDay19Part2(i)
+		expected := 0
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+}
+
+func BenchmarkSolveDay19Part2(b *testing.B) {
+	i, _ := ioutil.ReadFile("input.txt")
+	input := string(i)
+	for n := 0; n < b.N; n++ {
+		_ = SolveDay19Part2(input)
+	}
+}
+
+func TestStringListToSlice(t *testing.T) {
+	t.Run("Test stringListToSlice with valid list", func(t *testing.T) {
+		s := `string
+string 2
+string:3`
+		got := stringListToSlice(s)
+		expected := []string{"string", "string 2", "string:3"}
+		if !equalString(got, expected) {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+}
+
+func TestIntListToSlice(t *testing.T) {
+	t.Run("Test intListToSlice with valid list", func(t *testing.T) {
+		s := `123
+1234
+000
+00123`
+		got := intListToSlice(s)
+		expected := []int{123, 1234, 0, 123}
+		if !equalInt(got, expected) {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+	t.Run("Test intListToSlice with invalid list", func(t *testing.T) {
+		s := `123
+1234a
+000
+00123`
+		got := intListToSlice(s)
+		expected := []int{}
+		if !equalInt(got, expected) {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+}
+
+func equalString(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func equalInt(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func equalInt64(a, b []int64) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
