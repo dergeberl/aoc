bdb7f7974fcd3fe468cf76135cc141edb09edc6a
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2021-12-16 19:27:25 +0100

    add helper functions for int slices

--- utils/calc.go
+++ utils/calc.go
@@ -1,5 +1,7 @@
 package utils
 
+import "sort"
+
 //SumSlice returns the sum of all numbers of a slice
 func SumSlice(slice []int) int {
 	var sum int
@@ -7,6 +9,35 @@ func SumSlice(slice []int) int {
 		sum += slice[i]
 	}
 	return sum
+}
+
+//ProductSlice returns the product of all numbers of a slice
+//returns 1 if slice is empty
+//if the len of the slice is 1 it returns the number of this one item
+func ProductSlice(slice []int) int {
+	product := 1
+	for i := range slice {
+		product *= slice[i]
+	}
+	return product
+}
+
+//MinimumSlice returns the lowest number from the slice (0 if slice is empty)
+func MinimumSlice(slice []int) int {
+	if len(slice) == 0 {
+		return 0
+	}
+	sort.Ints(slice)
+	return slice[0]
+}
+
+//MaximumSlice returns the highest number from the slice (0 if slice is empty)
+func MaximumSlice(slice []int) int {
+	if len(slice) == 0 {
+		return 0
+	}
+	sort.Ints(slice)
+	return slice[len(slice)-1]
 }
 
 //GetDiff returns the difference between a and b
--- utils/calc_test.go
+++ utils/calc_test.go
@@ -12,3 +12,90 @@ func TestSumSlice(t *testing.T) {
 		}
 	})
 }
+
+func TestProductSlice(t *testing.T) {
+	t.Run("Test ProductSlice with valid list", func(t *testing.T) {
+		s := []int{10, 10, 10}
+		got := ProductSlice(s)
+		expected := 10 * 10 * 10
+		if got != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+
+	t.Run("Test ProductSlice with valid list", func(t *testing.T) {
+		s := []int{}
+		got := ProductSlice(s)
+		expected := 1
+		if got != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+
+	t.Run("Test ProductSlice with valid list", func(t *testing.T) {
+		s := []int{80}
+		got := ProductSlice(s)
+		expected := 80
+		if got != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+}
+
+func TestMaximumSliceSlice(t *testing.T) {
+	t.Run("Test MaximumSlice with valid list", func(t *testing.T) {
+		s := []int{10, 1, 5}
+		got := MaximumSlice(s)
+		expected := 10
+		if got != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+
+	t.Run("Test MaximumSlice with valid list", func(t *testing.T) {
+		s := []int{100, 123, 500, 2}
+		got := MaximumSlice(s)
+		expected := 500
+		if got != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+
+	t.Run("Test MaximumSlice with valid list", func(t *testing.T) {
+		s := []int{}
+		got := MaximumSlice(s)
+		expected := 0
+		if got != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+}
+
+func TestMinimumSlice(t *testing.T) {
+	t.Run("Test MaximumSlice with valid list", func(t *testing.T) {
+		s := []int{10, 1, 5}
+		got := MinimumSlice(s)
+		expected := 1
+		if got != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+
+	t.Run("Test MaximumSlice with valid list", func(t *testing.T) {
+		s := []int{100, 123, 500, 66}
+		got := MinimumSlice(s)
+		expected := 66
+		if got != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+
+	t.Run("Test MaximumSlice with valid list", func(t *testing.T) {
+		s := []int{}
+		got := MinimumSlice(s)
+		expected := 0
+		if got != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, got)
+		}
+	})
+}
