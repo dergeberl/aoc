24b4caf401fcecb956f24ecede8be718015f522a
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2021-12-21 16:48:45 +0100

    refactor day21

--- 2021/day21/day21.go
+++ 2021/day21/day21.go
@@ -15,8 +15,9 @@ type player struct {
 }
 
 type game struct {
-	player1 player
-	player2 player
+	player1       player
+	player2       player
+	currentPlayer int
 }
 
 func main() {
@@ -33,44 +34,25 @@ func main() {
 func SolveDay21Part1(input string) int {
 	g := parseInput(input)
 
-	curPlayer := 1
-	curNum := 0
-	dieCount := 0
-
+	var curNum, cur3Nums, dieCount int
 	for {
 		dieCount += 3
-		curNum++
-		if curNum > 100 {
-			curNum = 1
-		}
-		cur3Nums := curNum
+		cur3Nums, curNum = getNext3DiceRoles(curNum)
 
-		curNum++
-		if curNum > 100 {
-			curNum = 1
-		}
-		cur3Nums += curNum
-
-		curNum++
-		if curNum > 100 {
-			curNum = 1
-		}
-		cur3Nums += curNum
-
-		if curPlayer == 1 {
+		if g.currentPlayer == 1 {
 			g.player1.position += cur3Nums
 			for g.player1.position > 10 {
 				g.player1.position -= 10
 			}
-
 			g.player1.score += g.player1.position
 
 			if g.player1.score >= 1000 {
 				return (dieCount) * g.player2.score
 			}
-			curPlayer = 2
+			g.currentPlayer = 2
 			continue
 		}
+
 		g.player2.position += cur3Nums
 		for g.player2.position > 10 {
 			g.player2.position -= 10
@@ -81,7 +63,7 @@ func SolveDay21Part1(input string) int {
 		if g.player2.score >= 1000 {
 			return (dieCount) * g.player1.score
 		}
-		curPlayer = 1
+		g.currentPlayer = 1
 
 	}
 }
@@ -89,8 +71,7 @@ func SolveDay21Part1(input string) int {
 //SolveDay21Part2 returns the number of wins of the better performing input after play a strange universe round
 func SolveDay21Part2(input string) int64 {
 	g := parseInput(input)
-	c := make(map[[5]int][]int64)
-	p1, p2 := g.playUniverseMode(1, &c)
+	p1, p2 := g.playUniverseMode(nil)
 	if p1 > p2 {
 		return p1
 	}
@@ -98,16 +79,19 @@ func SolveDay21Part2(input string) int64 {
 }
 
 //playUniverseMode returns the number of wins for player1 and player2 after all possible games are played
-func (g game) playUniverseMode(pl int, cache *map[[5]int][]int64) (int64, int64) {
+func (g game) playUniverseMode(cache *map[game][2]int64) (int64, int64) {
 	if g.player1.score >= 21 {
 		return 1, 0
 	}
 	if g.player2.score >= 21 {
 		return 0, 1
 	}
+	if cache == nil {
+		cache = &map[game][2]int64{}
+	}
 
-	c, ok := (*cache)[[5]int{g.player1.position, g.player1.score, g.player2.position, g.player2.score, pl}]
-	if ok {
+	// check cache
+	if c, ok := (*cache)[g]; ok {
 		return c[0], c[1]
 	}
 
@@ -116,35 +100,57 @@ func (g game) playUniverseMode(pl int, cache *map[[5]int][]int64) (int64, int64)
 		for i2 := 1; i2 <= 3; i2++ {
 			for i3 := 1; i3 <= 3; i3++ {
 				newGame := g
-				if pl == 1 {
+				if g.currentPlayer == 1 {
 					newGame.player1.position = g.player1.position + i1 + i2 + i3
 					for newGame.player1.position > 10 {
 						newGame.player1.position -= 10
 					}
 					newGame.player1.score = g.player1.score + newGame.player1.position
-					tmpP1, tmpP2 := newGame.playUniverseMode(2, cache)
+					newGame.currentPlayer = 2
+					tmpP1, tmpP2 := newGame.playUniverseMode(cache)
 					p1 += tmpP1
 					p2 += tmpP2
+					continue
 				}
+				newGame.player2.position = g.player2.position + i1 + i2 + i3
+				for newGame.player2.position > 10 {
+					newGame.player2.position -= 10
+				}
+				newGame.player2.score = g.player2.score + newGame.player2.position
+				newGame.currentPlayer = 1
+				tmpP1, tmpP2 := newGame.playUniverseMode(cache)
+				p1 += tmpP1
+				p2 += tmpP2
 
-				if pl == 2 {
-					newGame.player2.position = g.player2.position + i1 + i2 + i3
-					for newGame.player2.position > 10 {
-						newGame.player2.position -= 10
-					}
-					newGame.player2.score = g.player2.score + newGame.player2.position
-					tmpP1, tmpP2 := newGame.playUniverseMode(1, cache)
-					p1 += tmpP1
-					p2 += tmpP2
-				}
 			}
 		}
 	}
 
-	(*cache)[[5]int{g.player1.position, g.player1.score, g.player2.position, g.player2.score, pl}] = make([]int64, 2)
-	(*cache)[[5]int{g.player1.position, g.player1.score, g.player2.position, g.player2.score, pl}][0] = p1
-	(*cache)[[5]int{g.player1.position, g.player1.score, g.player2.position, g.player2.score, pl}][1] = p2
+	//add to cache
+	(*cache)[g] = [2]int64{p1, p2}
 	return p1, p2
+}
+
+//getNext3DiceRoles returns the next sum of the next 3 dices and the next dice start point
+func getNext3DiceRoles(cur int) (next3numbers int, nextCur int) {
+	cur++
+	if cur > 100 {
+		cur = 1
+	}
+	next3numbers = cur
+
+	cur++
+	if cur > 100 {
+		cur = 1
+	}
+	next3numbers += cur
+
+	cur++
+	if cur > 100 {
+		cur = 1
+	}
+	next3numbers += cur
+	return next3numbers, cur
 }
 
 //parseInput returns a game for the input
@@ -175,5 +181,6 @@ func parseInput(input string) game {
 			}
 		}
 	}
+	g.currentPlayer = 1
 	return g
 }
